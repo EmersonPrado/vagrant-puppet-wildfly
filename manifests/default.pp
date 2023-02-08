bcf3694532c212e28706c7a5755e8b8171cb8465
Emerson Prado <emerson.prado.eng@gmail.com>
2023-02-08 17:21:07 -0300

    Install Wildfly

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,14 @@
+$tmp_dir = '/tmp/wildfly'
+
+file { $tmp_dir:
+  ensure => directory,
+}
+
 class { 'java':
   distribution => 'jdk',
+}
+
+class { 'wildfly':
+  install_cache_dir => $tmp_dir,
+  java_home         => '/usr',
 }
