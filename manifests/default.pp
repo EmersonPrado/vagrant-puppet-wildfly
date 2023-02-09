658d17fa6287c82325ce102bdd2f98739a5fbc35
Emerson Prado <emerson.prado.eng@gmail.com>
2023-02-09 18:06:00 -0300

    Set per-VM admin console bind IP

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,4 +1,7 @@
 $tmp_dir = '/tmp/wildfly'
+$standalone_ip = '192.168.56.4'
+$controller_ip = '192.168.56.5'
+$managed_ip = '192.168.56.6'
 
 file { $tmp_dir:
   ensure => directory,
@@ -13,9 +16,13 @@ $wildfly_defaults = {
   java_home         => '/usr',
 }
 
+# The Admin Console listens to the management interface
 node /^standalone/ {
   class { 'wildfly':
-    * => $wildfly_defaults,
+    properties => {
+      'jboss.bind.address.management' => $standalone_ip,
+    },
+    *          => $wildfly_defaults,
   }
 }
 
@@ -23,6 +30,9 @@ node /^controller/ {
   class { 'wildfly':
     mode        => 'domain',
     host_config => 'host-master.xml',
+    properties  => {
+      'jboss.bind.address.management' => $controller_ip,
+    },
     *           => $wildfly_defaults,
   }
 }
@@ -31,6 +41,10 @@ node /^managed/ {
   class { 'wildfly':
     mode        => 'domain',
     host_config => 'host-slave.xml',
+    properties  => {
+    'jboss.bind.address.management' => $managed_ip,
+      'jboss.domain.master.address' => $controller_ip,
+    },
     *           => $wildfly_defaults,
   }
 }
