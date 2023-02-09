c2447965ea2d7a08198e97ba86bd9f874a3aca82
Emerson Prado <emerson.prado.eng@gmail.com>
2023-02-09 18:03:48 -0300

    Add per-VM sections in main manifest

--- manifests/default.pp
+++ manifests/default.pp
@@ -8,7 +8,29 @@ class { 'java':
   distribution => 'jdk',
 }
 
-class { 'wildfly':
+$wildfly_defaults = {
   install_cache_dir => $tmp_dir,
   java_home         => '/usr',
+}
+
+node /^standalone/ {
+  class { 'wildfly':
+    * => $wildfly_defaults,
+  }
+}
+
+node /^controller/ {
+  class { 'wildfly':
+    mode        => 'domain',
+    host_config => 'host-master.xml',
+    *           => $wildfly_defaults,
+  }
+}
+
+node /^managed/ {
+  class { 'wildfly':
+    mode        => 'domain',
+    host_config => 'host-slave.xml',
+    *           => $wildfly_defaults,
+  }
 }
